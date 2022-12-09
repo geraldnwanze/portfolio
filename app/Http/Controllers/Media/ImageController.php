66a051aee3404e47b1640994af5b09918a44ef4e
geraldon1997 <geraldon1997@gmail.com>
2022-12-09 15:01:27 +0100

    required cloudinary into project

---
+++ app/Http/Controllers/Media/ImageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Media;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    public function upload()
+    {
+
+    }
+}
